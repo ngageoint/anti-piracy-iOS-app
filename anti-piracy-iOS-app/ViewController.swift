3c37f570abb25f20c9d35acf27ceb9ca9414727c
Travis Baumgart <baumgartt@bit-sys.com>
2015-02-26 13:14:50 -0700

    fresh xcode project

---
+++ anti-piracy-iOS-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  anti-piracy-iOS-app
+//
+//  Created by Travis Baumgart on 2/26/15.
+//  Copyright (c) 2015 NGA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
