3b1b5795bc67c19f23e1e1fc0d74f13ccef21051
Travis Baumgart <baumgartt@bit-sys.com>
2015-02-27 10:35:58 -0700

    Offline Map cleanup.

--- anti-piracy-iOS-app/ViewController.swift
+++ anti-piracy-iOS-app/ViewController.swift
@@ -7,19 +7,84 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let offlineMap:OfflineMap = OfflineMap()
+    
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        
+        var polygonRenderer = MKPolygonRenderer(overlay: overlay);
+        
+        if "ocean" == overlay.title {
+            polygonRenderer.fillColor = UIColor(red: 127/255.0, green: 153/255.0, blue: 171/255.0, alpha: 1.0)
+            polygonRenderer.strokeColor = UIColor.clearColor()
+            polygonRenderer.lineWidth = 0.0
+        }
+        else {
+            polygonRenderer.fillColor = UIColor(red: 221/255.0, green: 221/255.0, blue: 221/255.0, alpha: 1.0)
+            polygonRenderer.strokeColor = UIColor.clearColor()
+            polygonRenderer.lineWidth = 0.0
+        }
+        
+        return polygonRenderer
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func showLayerActionSheet(sender: UIButton) {
+        
+        // Action Sheet Label
+        let optionMenu = UIAlertController(title: nil, message: "Select Map Type", preferredStyle: .ActionSheet)
+        
+        // Action Sheet Options
+        let standardMapAction = UIAlertAction(title: "Standard", style: .Default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            self.mapView.mapType = MKMapType.Standard
+            self.mapView.removeOverlays(self.offlineMap.polygons)
+        })
+        let satelliteMapAction = UIAlertAction(title: "Satellite", style: .Default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            self.mapView.mapType = MKMapType.Satellite
+            self.mapView.removeOverlays(self.offlineMap.polygons)
+        })
+        let hybridMapAction = UIAlertAction(title: "Hybrid", style: .Default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            self.mapView.mapType = MKMapType.Hybrid
+            self.mapView.removeOverlays(self.offlineMap.polygons)
+        })
+        let offlineMapAction = UIAlertAction(title: "Offline", style: .Default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            self.mapView.mapType = MKMapType.Standard
+            self.mapView.addOverlays(self.offlineMap.polygons)
+        })
+        
+        // Action Sheet Cancel
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: {
+            (alert: UIAlertAction!) -> Void in
+            println("Cancelled")
+        })
+        
+        // Build Menu
+        optionMenu.addAction(standardMapAction)
+        optionMenu.addAction(satelliteMapAction)
+        optionMenu.addAction(hybridMapAction)
+        optionMenu.addAction(offlineMapAction)
+        optionMenu.addAction(cancelAction)
+        
+        // Show Action Sheet
+        self.presentViewController(optionMenu, animated: true, completion: nil)
+        
+    }
 
 }
 
