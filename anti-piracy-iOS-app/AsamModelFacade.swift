8aea5e9e9e30ed4bd3e80db2ab4adeaeb607c3e0
Chris Wasko <cwasko@caci.com>
2015-07-10 15:58:18 -0400

    Updated the filter to support subregion filters. In doing so I rewrote the filter logic to make it easier to add new ones in the future. Changed the "Now Showing..." text to grey with a light background, since the red was looking too much like an error. (Still need to make the background of the label not be as big)

--- anti-piracy-iOS-app/AsamModelFacade.swift
+++ anti-piracy-iOS-app/AsamModelFacade.swift
@@ -15,56 +15,95 @@ class AsamModelFacade {
 
     var asams = [Asam]()
     
+    let AND_PREDICATE = " and "
+    let OR_PREDICATE = " or "
     let defaults = NSUserDefaults.standardUserDefaults()
 
     
     func getAsams()-> Array<Asam> {
-    
-        
-        let appDelegate =
-        UIApplication.sharedApplication().delegate as! AppDelegate
-        
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         let managedObjectContext = appDelegate.managedObjectContext!
-
         // Create a new fetch request using the LogItem entity
         let fetchRequest = NSFetchRequest(entityName: "Asam")
         
-        //build predicate for query
-        var filterNames: [String] = []
-        var filterValues: [AnyObject] = []
-        
-        //start date filter
-        if let userDefaultStartDate: NSDate = defaults.objectForKey("startDate") as? NSDate {
-            filterNames.append("(date > %@)")
-            filterValues.append(userDefaultStartDate)
-        }
-        
-        //end date filter
-        if let userDefaultEndDate: NSDate = defaults.objectForKey("endDate") as? NSDate {
-            filterNames.append("(date < %@)")
-            filterValues.append(userDefaultEndDate)
-        }
-        
-        //build predicate string
-        var predicateString = String()
-        if filterNames.count > 0 {
-            for index in 0...(filterNames.count-1) {
-                predicateString += filterNames[index]
-                if index < filterNames.count-1 {
-                    predicateString += " and "
-                }
-            }
-            
-            let pred = NSPredicate(format: predicateString, argumentArray: filterValues)
-            fetchRequest.predicate = pred
-            
-        }
+        fetchRequest.predicate = getFilterPredicate()
         
         let fetchResults = managedObjectContext.executeFetchRequest(fetchRequest, error: nil) as? [Asam]
         asams = fetchResults!
         
         return asams
+    }
+    
+    
+    func getFilterPredicate() -> NSPredicate {
         
+        var filterPredicate = getDatePredicate()
+
+        if let subregionPredicate = getSubregionPredicate() {
+            filterPredicate = NSCompoundPredicate(type: NSCompoundPredicateType.AndPredicateType, subpredicates: [filterPredicate, subregionPredicate])
+        }
+       
+        return filterPredicate
+    }
+    
+    
+    func getDatePredicate() -> NSPredicate {
+        var dateNames: [String] = []
+        var dateValues: [AnyObject] = []
+        
+        if let userDefaultStartDate: NSDate = defaults.objectForKey("startDate") as? NSDate {
+            dateNames.append("(date > %@)")
+            dateValues.append(userDefaultStartDate)
+        }
+        
+        if let userDefaultEndDate: NSDate = defaults.objectForKey("endDate") as? NSDate {
+            dateNames.append("(date < %@)")
+            dateValues.append(userDefaultEndDate)
+        }
+        
+        //build predicate string
+        let datePredicateFormat = buildPredicateFormat(AND_PREDICATE, names: dateNames)
+        
+        var datePredicate = NSPredicate(format: datePredicateFormat, argumentArray: dateValues)
+
+        return datePredicate
+    }
+
+    
+    func getSubregionPredicate() -> NSPredicate? {
+        var subregionPredicate: NSPredicate? = nil
+        var regionNames = [String]()
+        var regionValues = [AnyObject]()
+        
+        if let userDefaultSubRegion: Array<String> = defaults.objectForKey("selectedRegions") as? Array<String> {
+            if userDefaultSubRegion.count > 0 {
+                for (region) in userDefaultSubRegion {
+                    regionNames.append("(subregion == %i)")
+                    regionValues.append(region.toInt()!)
+                }
+                
+                let subregionPredicateFormat = buildPredicateFormat(OR_PREDICATE, names: regionNames)
+                subregionPredicate = NSPredicate(format: subregionPredicateFormat, argumentArray: regionValues)
+            }
+        }
+
+        return subregionPredicate
+    }
+    
+    
+    func buildPredicateFormat(predicateType: String, names: [String]) -> String {
+        var predicateFormat = String()
+        if names.count > 0 {
+            for index in 0..<(names.count) {
+                predicateFormat += names[index]
+                if index < names.count-1 {
+                    predicateFormat += predicateType
+                }
+            }
+            
+        }
+        
+        return predicateFormat
     }
     
 }
